b83fd62dda6e36ad51134d77f3bdacb6fe423902
Eugenie O. <eugenie@ordonneau.com>
2025-12-30 20:00:43 +0100

    make player move around

--- player.py
+++ player.py
@@ -2,7 +2,8 @@ from circleshape import CircleShape
 from constants import (
     PLAYER_RADIUS, 
     LINE_WIDTH,
-    PLAYER_TURN_SPEED
+    PLAYER_TURN_SPEED,
+    PLAYER_SPEED
 )
 import pygame
 
@@ -30,6 +31,12 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        unit_vector = pygame.Vector2(0, 1)
+        rotated_vector = unit_vector.rotate(self.rotation)
+        rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
+        self.position += rotated_with_speed_vector
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
@@ -37,4 +44,8 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
